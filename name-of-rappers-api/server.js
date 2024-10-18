6c42bb871eb275d2381553a3ac5e0a18efaecc1b
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-10-18 10:50:05 +0100

    update sever

--- name-of-rappers-api/server.js
+++ name-of-rappers-api/server.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const PORT = 8000
 
+const savage = {
+    'age': 35,
+    'birthName': 'Nurudeen Adewale Adedeji',
+    'favoriteFood': 'Lamb',
+    'hobbies': ['Reading', 'Cooking', 'Running'],
+    'family': ['Mum', 'Dad', 'Brother'],
+    'friends': ['Adewole', 'Ogundele', 'Ezekiel'],
+    'occupation': 'Engineering',
+    'BirthLocation': 'London,England'
+}
+
 // Middleware to parse JSON request bodies
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
-
+app.get('/api', (req, res) => {
+    res.json()
+})
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
