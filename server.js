9598134955a69cff8e72e6a3322152a17ee9e916
Nurudeen Adedeji <djsmacker01@gmail.com>
2024-10-18 10:27:12 +0100

    update server

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
-
+const PORT = 8000
 
 // Middleware to parse JSON request bodies
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
